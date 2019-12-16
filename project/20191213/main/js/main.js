52c3d703ead312f573c7e17cf95a58d120a958c0
Jams <xjamsx@gmail.com>
2019-12-16 13:57:09 +0800

    updated bdg

--- project/20191213/main/js/main.js
+++ project/20191213/main/js/main.js
@@ -1,23 +1,45 @@
 $(function() {
   var ripple_tf = false;
+  var formNum = 1;
   var skrollr_obj = skrollr.init({
     render: function(data) {
-            //Log the current scroll position.
-            // console.log(data.curTop);
-            if(data.curTop>=9700){
-              if(ripple_tf==false){
-                ripple_tf = true;
-                $('.ripple__holder').ripples({
-              	resolution: 512,
-              	dropRadius: 25,
-              	perturbance: 0.04,
-                interactive:false,
-                crossOrigin:"anonymous",
-                imageUrl:"img/bg_kai.jpg"
-                });
-              }
-            }
+      //Log the current scroll position.
+      // console.log(data.curTop);
+      if (data.curTop >= 9700) {
+        if (ripple_tf == false) {
+          ripple_tf = true;
+          $('.ripple__holder').ripples({
+            resolution: 512,
+            dropRadius: 25,
+            perturbance: 0.04,
+            interactive: false,
+            crossOrigin: "Anonymous",
+            imageUrl: "img/bg_kai.jpg"
+          });
         }
+      }
+      if (data.curTop >= 12300) {
+
+        switch (formNum) {
+          case 1:
+            $('.form__step1').show();
+            $('.form__step2').hide();
+            $('.form__step3').hide();
+
+            break;
+          case 2:
+            $('.form__step1').hide();
+            $('.form__step2').show();
+            $('.form__step3').hide();
+            break;
+          case 3:
+            $('.form__step1').hide();
+            $('.form__step2').hide();
+            $('.form__step3').show();
+            break;
+        }
+      }
+    }
   });
   var skrollr_menu = skrollr.menu.init(skrollr_obj, {
     change: function(hash, top) {
@@ -40,26 +62,83 @@ $(function() {
   }
   img_cover.src = "img/tri_mask.png";
 
-  var worldTime = setInterval(drawWorld, 50);
+  var wt = 0;
+  var worldTime = setInterval(drawWorld, 10);
 
   function drawWorld() {
-    if (img_ready == true) {
-      ktx.drawImage(img_cover, 0, 0);
-      ktx.globalCompositeOperation = 'source-in';
-      ktx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, 400, 400);
+    if (wt % 5 == 0) {
+      if (img_ready == true) {
+        ktx.drawImage(img_cover, 0, 0);
+        ktx.globalCompositeOperation = 'source-in';
+        ktx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, 400, 400);
+      }
+    }
+    if (wt % 70 == 0) {
+      if (ripple_tf == true) {
+        var $el = $('.ripple__holder');
+        var x = Math.random() * $el.outerWidth();
+        var y = Math.random() * $el.outerHeight();
+        var dropRadius = 20;
+        var strength = 0.04 + Math.random() * 0.04;
+
+        $el.ripples('drop', x, y, dropRadius, strength);
+      }
+    }
+    wt++;
+    if (wt > 7000) {
+      wt = 0;
     }
   }
+  //form
+  AddressSeleclList.Initialize('select__city', 'select__area', "選擇縣市", "選擇區域");
 
-  setInterval(function() {
-    if(ripple_tf==true){
-      var $el = $('.ripple__holder');
-      var x = Math.random() * $el.outerWidth();
-      var y = Math.random() * $el.outerHeight();
-      var dropRadius = 20;
-      var strength = 0.04 + Math.random() * 0.04;
+  $("#button__addr").on("click", function() {
+    $('.form__step1').hide();
+    $('.form__step2').show();
+    formNum = 2;
+  })
 
-      $el.ripples('drop', x, y, dropRadius, strength);
+  $('#select__area').change(function() {
+    appendStore();
+  })
 
-  }}, 700);
+  function appendStore() {
 
+  }
+  $('#button__confirm').on('click', function() {
+    var ck = $('[name=store]:checked');
+    addr = $('label[for=' + ck.val() + ']')[0].innerText;
+    $('#form__addr').val(addr);
+    $('.form__step1').show();
+    $('.form__step2').hide();
+    formNum = 1;
+  })
+  $('#button__reset').on('click', function() {
+
+  })
+  //submit表單
+  $("#button__submit").on('click', function() {
+    $('#form__name').val()
+    //checkForm();
+    step3();
+  })
+  //補寄簡訊
+  $('#resent__msg').on('click',function(){
+
+  })
+
+  function step3(){
+      formNum = 3;
+      $('.form__step1').hide();
+      $('.form__step2').hide();
+      $('.form__step3').show();
+  }
+
+  function checkForm() {
+    if ($('#form__name').val() == "" || $('#form__tel').val() == "" || $('#form__email').val() == "" || $('#form__addr').val() == "") {
+      console.log('please check the form again');
+    } else {
+      step3();
+    }
+  }
 });
