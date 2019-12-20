867c11d23fbf3b43cb06399ee7418b472221f374
Jams <xjamsx@gmail.com>
2019-12-20 17:53:15 +0800

    bdg update font

--- project/20191220/main/js/main.js
+++ project/20191220/main/js/main.js
@@ -123,9 +123,8 @@ $(function() {
     }
   });
   var skrollr_obj = skrollr.init({
+
     render: function(data) {
-      //Log the current scroll position.
-      // console.log(data.curTop);
 
       nowTop = data.curTop;
       if (data.curTop >= 6200) {
@@ -274,3 +273,16 @@ $(function() {
     }
   }
 });
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: null };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  window.CustomEvent = CustomEvent;
+})();
