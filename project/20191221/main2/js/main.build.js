547de766224ef72bcd8a587176ea446fd46921ef
Jams <xjamsx@gmail.com>
2019-12-23 02:24:11 +0800

    bdg updeted

--- project/20191221/main2/js/main.build.js
+++ project/20191221/main2/js/main.build.js
@@ -2,28 +2,33 @@ $(document).ready(function() {
 
   var stgW = window.innerWidth;
   var stgH = window.innerHeight;
-  var kalW,kalH,kalML,kalMT;
-  var rate = 3/5;
-  if(stgW > stgH ){
+  var kalW, kalH, kalML, kalMT;
+  var rate = 3 / 5;
+  if (stgW > stgH) {
     kalW = kalH = stgW;
-    kalMT =((stgH*rate)- kalH/2) ;
+    kalMT = ((stgH * rate) - kalH / 2);
     kalML = 0;
-    console.log('kalML'+kalML);
-  }else{
-    kalW = kalH = stgH  *2 *rate;
-    kalML = (stgW-kalW)/2;
+    console.log('kalML' + kalML);
+  } else {
+    kalW = kalH = stgH * 2 * rate;
+    kalML = (stgW - kalW) / 2;
     kalMT = 0;
   }
-  $("#kal").css({"height": kalH+"px", "width": kalW+"px","margin-top":kalMT+"px","margin-left":kalML+"px"});
+  $("#kal").css({
+    "height": kalH + "px",
+    "width": kalW + "px",
+    "margin-top": kalMT + "px",
+    "margin-left": kalML + "px"
+  });
 
-  var xx =0;
-  var yy =0;
-  var spdX=1;
-  var spdY=1;
-  var deg=0;
+  var xx = 0;
+  var yy = 0;
+  var spdX = 1;
+  var spdY = 1;
+  var deg = 0;
   var rad;
-  var xlimit = window.innerWidth/4;
-  var ylimit = window.innerHeight/4;
+  var xlimit = window.innerWidth / 4;
+  var ylimit = window.innerHeight / 4;
 
   var timer = setInterval(function() {
     $(".kal_cont").each(function(i) {
@@ -34,17 +39,36 @@ $(document).ready(function() {
         });
       });
     });
-    rad = Math.PI/180*deg;
-    spdY = Math.sin(rad)*ylimit/2;
-    spdX = Math.cos(rad)*xlimit/2;
+    rad = Math.PI / 180 * deg;
+    spdY = Math.sin(rad) * ylimit / 2;
+    spdX = Math.cos(rad) * xlimit / 2;
     deg++;
 
-    xx=spdX;
-    yy=spdY;
+    xx = spdX;
+    yy = spdY;
   }, 50);
-  skrollr.init({
-  		forceHeight: false,
-       smoothScrolling : true,
-       smoothScrollingDuration:200,
+  var s = skrollr.init({
+    forceHeight: false,
+    smoothScrolling: true,
+    smoothScrollingDuration: 200,
+  });
+  skrollr.menu.init(s, {
+    animate: true,
+    easing: 'sqrt',
+    duration: function(currentTop, targetTop) {
+
+      return Math.abs(currentTop - targetTop) * 0.5;
+    },
+    // handleLink: function(link) {
+    //   return 400;
+    // },
+
+    complexLinks: false,
+
+    change: function(newHash, newTopPosition) {
+
+    },
+
+    updateUrl: false //defaults to `true`.
   });
 });
